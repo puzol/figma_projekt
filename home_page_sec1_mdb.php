b52386b5956cf5a47151c57ee9800610dac75164
puzol <51291320+puzol@users.noreply.github.com>
2020-08-09 22:18:49 +0200

    updated header for bootstrap

---
+++ home_page_sec1_mdb.php
@@ -0,0 +1,12 @@
+
+
+<section class="default_section">
+    <div class="hero">
+        <h1 class="gradient_title">#WEEE4Future</h1>
+        <p>
+            Keyvisual<br>
+            <span>What is WEEE<br>
+            Campaign idea explained</span>
+        </p>
+    </div>   
+</section>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
     //include_once('header.php');
     include_once('home_page_sec1.php');
+    include_once('home_page_sec1_mdb.php');
     include_once('home_page_sec2.php');
     include_once('footer.php');
 ?>
@@ -10,29 +11,25 @@
     <head>
         <title>WEE4Future</title>
         <link rel="stylesheet" href="style.css">
-        <!-- Font Awesome -->
-        <link
-        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        rel="stylesheet"
-        />
-        <!-- Google Fonts -->
-        <link
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        rel="stylesheet"
-        />
-        <!-- MDB -->
-        <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0-alpha4/mdb.min.css"
-        rel="stylesheet"
-        />
-        <script
-        type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0-alpha4/mdb.min.js"
->       </script>
+         <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="css/mdb.min.css" rel="stylesheet">
         <meta charset="utf-8">
     </head>
 
     <body>
       
+        <!-- SCRIPTS -->
+        <!-- JQuery -->
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="js/mdb.min.js"></script>
     </body>
 </html>
